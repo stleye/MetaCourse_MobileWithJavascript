bb9ad01d7f1a7f8e3b3c6c55b6cedf93751c9016
Sebastian Tleye <stleye@gmail.com>
2024-02-02 09:04:42 -0300

    Added more exercises

--- switch_and_if.js
+++ switch_and_if.js
@@ -36,3 +36,21 @@ switch (day) {
         console.log("There is no such day")
         break;
 }
+
+
+for (var i = 1; i <= 10; i++) {
+    switch (i) {
+        case 1:
+            console.log("Gold Medal");
+            break;
+        case 2:
+            console.log("Silver Medal");
+            break;
+        case 3:
+            console.log("Bronze Medal");
+            break;
+        default:
+            console.log(i);
+            break;
+    }
+}
