928d4a556a5a8584a758c6e893ee05838810ae1b
Sebastian Tleye <stleye@gmail.com>
2024-02-02 10:20:10 -0300

    Added simple_functions and array_and_objects

---
+++ arrays_and_objects.js
@@ -0,0 +1,17 @@
+var clothes = []
+clothes.push("t-shirt")
+clothes.push("muscle t-shirt")
+clothes.push("shoes")
+clothes.push("pants")
+clothes.push("underwear")
+
+clothes.pop()
+
+clothes.push("shorts")
+
+console.log(clothes[2]);
+
+var favCar = {}
+favCar.color = "Green"
+favCar.covertible = true
+console.log(favCar)
