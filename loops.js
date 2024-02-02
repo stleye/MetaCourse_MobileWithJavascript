f17b0661a9703429e1da9f32c30077f1f292e1c5
Sebastian Tleye <stleye@gmail.com>
2024-02-01 22:23:32 -0300

    Added loops.js

---
+++ loops.js
@@ -0,0 +1,29 @@
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting completed!')
+
+for (var i = 5; i >= 1; i--) {
+    console.log(i)
+}
+console.log('Countdown finished!')
+
+var counter = 1;
+while (counter <= 5) {
+    console.log(counter);
+    counter += 1
+}
+console.log('Counting completed!')
+
+var counter = 5;
+while (counter >= 1) {
+    console.log(counter);
+    counter -= 1
+}
+console.log('Countdown finished!')
+
+var counter = 2018;
+while (counter <= 2022) {
+    console.log(counter);
+    counter += 1
+}
